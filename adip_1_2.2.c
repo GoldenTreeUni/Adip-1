7cd564d19237889333fe29bd93405f51967ca085
GoldenTreeUni <michaelwp91@googlemail.com>
2018-10-25 00:50:48 +0200

    second file

---
+++ adip_1_2.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+int square_number;
+int i;
+printf("Gebe eine positive ganze zahl ein");
+scanf("%d",&i);
+square_number = i*i;
+printf("Das Ergebnis lautet %d",square_number);
+}
