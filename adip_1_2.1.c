6682e726f67961a4033c2a1020c32b50a5697bd2
GoldenTreeUni <michaelwp91@googlemail.com>
2018-10-25 00:44:42 +0200

    first file

---
+++ adip_1_2.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    int c=0;
+    printf("Gebe zwei Positive Nummbers ein");
+    scanf("%d%d",&a,&b);
+    c= a+b;
+    printf("Das Ergebnis lautet %d",c);
+}
+
